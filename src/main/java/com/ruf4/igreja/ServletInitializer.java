d5230ef501aae9a8ce5b18ecb3e9d4b159b5d375
Leandro-Ruas <52423618+Leandro-Ruas@users.noreply.github.com>
2022-01-04 18:03:32 -0300

    Projeto criado

---
+++ src/main/java/com/ruf4/igreja/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ruf4.igreja;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(IgrejaApplication.class);
+	}
+
+}
